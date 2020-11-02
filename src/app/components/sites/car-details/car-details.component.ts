0f613d9f161df5aa46ae319b163e556016dc4156
Rein Oterholm <rein@otter.productions>
2020-11-02 05:00:21 +0100

    added subscription service

--- src/app/components/sites/car-details/car-details.component.ts
+++ src/app/components/sites/car-details/car-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { BackendService } from '@services';
 
 @Component({
   selector: 'app-car-details',
@@ -11,22 +13,31 @@ export class CarDetailsComponent implements OnInit {
   car:any;
   sub:any;
   minDate:Date;
+  subscriptionForm:FormGroup;
+  error:string;
+
 
   constructor(
       private route: ActivatedRoute,
       private router:Router,
-      private http: HttpClient
+      private http: HttpClient,
+      private backendService: BackendService
   ) {
       this.sub = route.params.subscribe(p => this.initCar(p['id']))
+      this.subscriptionForm = new FormGroup({
+          deliveryDate: new FormControl('', [Validators.required]),
+          address: new FormControl('', [Validators.required]),
+      })
   }
 
   ngOnInit(): void {
       this.minDate = new Date();
   }
-  
+
   ngOnDestroy(){
       this.sub.unsubscribe();
   }
+
   initCar(id){
       this.http.get(`/api/car-type/id/${id}`)
         .subscribe((doc) => {
@@ -34,5 +45,25 @@ export class CarDetailsComponent implements OnInit {
             this.car = doc;
         })
   }
+
+  subscribe(){
+      this.error = undefined;
+      if(this.subscriptionForm.status == "INVALID") return this.error = "Please fill out the form";
+
+      this.http.put(`/api/subscription`, {
+          deliveryDate: this.subscriptionForm.value['deliveryDate'],
+          address: this.subscriptionForm.value['address'],
+          car: this.car._id
+      })
+      .subscribe((subscription:any) => {
+          this.router.navigate(['/subscription', subscription._id])
+      }, (e) => {
+          this.error = this.backendService.formatHttpError(e);
+      })
+
+
+  }
+
+
 
 }
