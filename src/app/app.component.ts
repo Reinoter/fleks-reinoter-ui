90036eeb770a8447acd2a069a27e3d5b0ba6d68d
Rein Oterholm <rein@otter.productions>
2020-11-02 08:44:01 +0100

    fixed scroll top

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import { CookieService } from 'ngx-cookie-service';
 import { BackendService } from "@services";
 import { fadeAnimation } from '@animations';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
     selector: 'app-root',
@@ -12,14 +13,30 @@ import { RouterOutlet } from '@angular/router';
     animations: [fadeAnimation]
 })
 export class AppComponent {
+    sub:any; //Refrence to router observable
     constructor(
         private http:HttpClient,
         private cookieService: CookieService,
-        private backendService: BackendService
-    ){}
+        private backendService: BackendService,
+        private router: Router
+    ){
+        //Making sure browser scrolls to the top of website on navigation.
+        this.sub = this.router.events
+        .pipe(
+            filter(event => event instanceof NavigationEnd)
+        )
+        .subscribe(event => {
+            var el = document.getElementById("page");
+            window.scroll(0, 0);
+        });
+    }
+
+    ngOnDestroy(){
+        this.sub.unsubscribe();
+    }
 
     /* Triggers router animation */
     prepareRoute(outlet: RouterOutlet) {
-      return outlet && outlet.activatedRouteData && outlet.activatedRouteData.animation;
+        return outlet && outlet.activatedRouteData && outlet.activatedRouteData.animation;
     }
 }
